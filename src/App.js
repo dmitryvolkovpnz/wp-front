6956a897496802b4e57eee83ea66f0c0c70c6444
DmitryHome <dvolkov0422@gmail.com>
2024-10-02 21:52:51 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import PostList from "./PostList";
 import PostDetail from "./PostDetail";
 import Login from "./Login";
 import NewPost from "./NewPost";
-import Footer from "./Footer";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   const [token, setToken] = useState(null);
@@ -17,17 +17,15 @@ function App() {
   return (
       <BrowserRouter>
         {token ? <div className="App container">
-          <Header />
+          <Header/>
           <Routes>
             <Route path="/" element={<PostList/>}/>
-            <Route path="/login" element={<Login/>}/>
             <Route path="/newpost" element={<NewPost token={token}/>}/>
             <Route path="/post/:id" element={<PostDetail/>}/>
           </Routes>
           <Footer/>
-        </div> : <div>
-          <Login/>
-        </div>}
+        </div> :
+            <Login/>}
       </BrowserRouter>
   );
 }
